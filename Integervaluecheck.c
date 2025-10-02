25ee0c806a71af2fe24aff15ae9184f2790fcbae
SurajKatiyar <surajkatiyar753@gmail.com>
2025-10-02 19:43:32 +0530

    Create Integervaluecheck.c

---
+++ Integervaluecheck.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+    float x;
+    printf("Enter a no which you want to check : \n");
+    scanf("%f", &x);
+
+    if (x > 0){
+        printf ("number is positive");
+
+    }
+    else if (x = 0)
+    {
+        printf("number is Zero");
+    }
+    else 
+    {
+        printf("Number is negative");
+    }
+    
+
+}
